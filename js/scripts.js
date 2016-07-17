0d9b2a8cc15fbe27e4187e065ac90770139818cd
Sahariar <sahariark@gmail.com>
2016-07-17 18:58:43 +0600

    update script js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 var canvas = document.getElementById('our-canvas'), // Our canvas element
     context = canvas.getContext('2d'); // 2d is the way to go here.
 
-context.fillRect(25, 25, 25, 25);
+
+canvas.width = canvas.width;
+context.beginPath();
+context.moveTo(200,150);
+context.lineTo(250,185);
+context.moveTo(215,190);
+context.lineTo(154,170);
+context.strokeStyle = '#a53124';
+context.stroke();
